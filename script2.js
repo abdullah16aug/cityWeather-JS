4f887aada0a6f09cff47709f368cf950b5f36233
Abdullah Asad <74613117+abdullah16aug@users.noreply.github.com>
2023-02-17 17:26:36 +0530

    entered background color functionality

--- script2.js
+++ script2.js
@@ -3,6 +3,33 @@ let condition = document.getElementById("condition");
 let gust_kph = document.getElementById("gust_kph");
 let country = document.getElementById("country");
 let localTime = document.getElementById("localTime");
+let element = document.getElementById("section");
+const changeColor=async(weather)=>{
+  if (weather == "Overcast") {
+    element.style.backgroundColor = "#001a49";
+  } else if (weather == "Clouds") {
+    element.style.backgroundColor = "blue";
+    // give proper url for the image or gif here
+  } else if (weather == "Clear") {
+    element.style.backgroundColor = "#00d1ff";
+
+  } else if (weather == "Haze") {
+    element.style.backgroundColor = "white";
+
+  } 
+  else if (weather == "Partly cloudy") {
+    element.style.backgroundColor = "#ed5555";
+
+  }
+  else if (weather == "Sunny") {
+    element.style.backgroundColor = "#ff7504";
+
+  }
+  else {
+    element.style.backgroundColor = "#262323";
+      // give proper url for the image or gif here
+  }
+}
 const getWeather = (city) => {
   cityName.innerHTML = city;
   fetch(
@@ -17,6 +44,7 @@ const getWeather = (city) => {
       country.innerHTML=response.location.country
       localTime.innerHTML=response.location.localtime
         console.log(response.current.gust_kph)
+        changeColor(response.current.condition.text)
     });
 };
 
